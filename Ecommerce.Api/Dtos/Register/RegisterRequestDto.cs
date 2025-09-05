09334de77471734c9c50824aab0082b313ff8502
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2025-09-05 17:53:38 +0545

    add Role property to RegisterRequestDto for user role assignment

--- Ecommerce.Api/Dtos/Register/RegisterRequestDto.cs
+++ Ecommerce.Api/Dtos/Register/RegisterRequestDto.cs
@@ -5,5 +5,6 @@ public class RegisterRequestDto
     public required string UserName { get; set; }
     public required string Name { get; set; }
     public required string Password { get; set; }
+    public required string Role { get; set; }
    
 }
