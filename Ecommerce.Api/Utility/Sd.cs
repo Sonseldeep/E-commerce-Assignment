728d85108a664950d7cc5a0440b49746bdfa13ff
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2025-09-07 13:16:33 +0545

    add order status constants for order management

--- Ecommerce.Api/Utility/Sd.cs
+++ Ecommerce.Api/Utility/Sd.cs
@@ -6,4 +6,11 @@ public static class Sd
     public const string SdStorageContainer = "redmango";
     public const string RoleAdmin = "admin";
     public const string RoleCustomer = "customer"; 
+    
+    public const string statusPending = "Pending";
+    public const string statusConfirmed = "Confirmed";
+    public const string statusBeingCooked = "Being Cooked";
+    public const string statusReadyForPickup = "Ready For Pickup";
+    public const string statusCompleted = "Completed";
+    public const string statusCancelled = "Cancelled";
 }
