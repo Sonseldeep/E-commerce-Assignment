ca9bca4fbf1a78f22ea1fd2cd0770491366d64b0
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2025-09-24 15:43:55 +0545

    refactor ShoppingCartController for improved readability and structure

--- Ecommerce.Api/Controllers/ShoppingCartController.cs
+++ Ecommerce.Api/Controllers/ShoppingCartController.cs
@@ -1,139 +1,131 @@
-using System.Net;
-using Ecommerce.Api.Data;
-using Ecommerce.Api.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-
+using System.Net; using Ecommerce.Api.Data; using Ecommerce.Api.Models; using Microsoft.AspNetCore.Mvc; using Microsoft.EntityFrameworkCore;
 namespace Ecommerce.Api.Controllers;
-
-[ApiController]
-public class ShoppingCartController : ControllerBase
+[ApiController] public class ShoppingCartController : ControllerBase { private readonly ApplicationDbContext _context; private readonly ApiResponse _response;
+public ShoppingCartController(ApplicationDbContext context)
 {
-    private readonly ApplicationDbContext _context;
-    private readonly ApiResponse _response;
+    _response = new ApiResponse();
+    _context = context;
+}
 
-    public ShoppingCartController(ApplicationDbContext context)
+[HttpGet("api/shoppingcart")]
+public async Task<ActionResult<ApiResponse>> GetShoppingCart(string userId)
+{
+    try
     {
-        _response = new ApiResponse();
-        _context = context;
+        ShoppingCart shoppingCart;
+        if (string.IsNullOrEmpty(userId))
+        {
+            shoppingCart = new();
+        }
+        else
+        {
+             shoppingCart =   _context.ShoppingCarts.Include(x => x.CartItems).ThenInclude(x => x.MenuItem)
+                 .FirstOrDefault(x => x.UserId == userId)!;
+        }
+
+        
+
+        if (shoppingCart?.CartItems is not null && shoppingCart.CartItems.Count > 0)
+        {
+            shoppingCart.CartTotal = shoppingCart.CartItems.Sum(x => x.Quantity * x.MenuItem.Price);
+        }
+        
+        _response.Result = shoppingCart!;
+        _response.StatusCode = HttpStatusCode.OK;
+        return Ok(_response);
+
+    }
+    catch (Exception ex)
+    {
+        _response.IsSuccess = false;
+        _response.ErrorMessages = [ex.ToString()];
+        _response.StatusCode = HttpStatusCode.BadRequest;
+    }
+    return _response;
+}
+
+[HttpPost("api/shoppingcart")]
+public async Task<ActionResult<ApiResponse>> AddOrUpdateItemInCart(string userId, int menuItemId, int updateQuantityBy)
+{
+    var shoppingCart = await _context.ShoppingCarts
+        .Include(x => x.CartItems)
+        .FirstOrDefaultAsync(s => s.UserId == userId);
+
+    var menuItem = await _context.MenuItems.FirstOrDefaultAsync(s => s.Id == menuItemId);
+
+    if (menuItem is null)
+    {
+        _response.StatusCode = HttpStatusCode.BadRequest;
+        _response.IsSuccess = false;
+        _response.ErrorMessages.Add("Invalid MenuItemId");
+        return BadRequest(_response);
     }
 
-    [HttpGet("api/shoppingcart")]
-    public async Task<ActionResult<ApiResponse>> GetShoppingCart(string userId)
+    if (shoppingCart is null && updateQuantityBy > 0)
     {
-        try
+        // create a new shopping cart
+        ShoppingCart newCart = new() { UserId = userId };
+        _context.ShoppingCarts.Add(newCart);
+        await _context.SaveChangesAsync();
+
+        CartItem newCartItem = new()
         {
-            if (string.IsNullOrEmpty(userId))
-            {
-                _response.IsSuccess = false;
-                _response.StatusCode = HttpStatusCode.BadRequest;
-                return BadRequest(_response);
-            }
+            MenuItemId = menuItemId,
+            Quantity = updateQuantityBy,
+            ShoppingCartId = newCart.Id
+        };
 
-            var shoppingCart = await  _context.ShoppingCarts.Include(x => x.CartItems).ThenInclude(x => x.MenuItem)
-                .FirstOrDefaultAsync(x => x.UserId == userId);
+        _context.CartItems.Add(newCartItem);
+        await _context.SaveChangesAsync();
 
-            if (shoppingCart?.CartItems is not null && shoppingCart.CartItems.Count > 0)
-            {
-                shoppingCart.CartTotal = shoppingCart.CartItems.Sum(x => x.Quantity * x.MenuItem.Price);
-            }
-            
-            _response.Result = shoppingCart!;
-            _response.StatusCode = HttpStatusCode.OK;
-            return Ok(_response);
-
-        }
-        catch (Exception ex)
-        {
-            _response.IsSuccess = false;
-            _response.ErrorMessages = [ex.ToString()];
-            _response.StatusCode = HttpStatusCode.BadRequest;
-        }
-        return _response;
+        _response.StatusCode = HttpStatusCode.OK;
+        _response.IsSuccess = true;
+        _response.Result = newCart;
+        return Ok(_response);
     }
 
-    [HttpPost("api/shoppingcart")]
-    public async Task<ActionResult<ApiResponse>> AddOrUpdateItemInCart(string userId, int menuItemId, int updateQuantityBy)
+    if (shoppingCart is not null)
     {
-        var shoppingCart = await _context.ShoppingCarts
-            .Include(x => x.CartItems)
-            .FirstOrDefaultAsync(s => s.UserId == userId);
-
-        var menuItem = await _context.MenuItems.FirstOrDefaultAsync(s => s.Id == menuItemId);
-
-        if (menuItem is null)
+        var cartItemInCart = shoppingCart.CartItems.FirstOrDefault(x => x.MenuItemId == menuItemId);
+        if (cartItemInCart is null && updateQuantityBy > 0)
         {
-            _response.StatusCode = HttpStatusCode.BadRequest;
-            _response.IsSuccess = false;
-            _response.ErrorMessages.Add("Invalid MenuItemId");
-            return BadRequest(_response);
-        }
-
-        if (shoppingCart is null && updateQuantityBy > 0)
-        {
-            // create a new shopping cart
-            ShoppingCart newCart = new() { UserId = userId };
-            _context.ShoppingCarts.Add(newCart);
-            await _context.SaveChangesAsync();
-
             CartItem newCartItem = new()
             {
                 MenuItemId = menuItemId,
                 Quantity = updateQuantityBy,
-                ShoppingCartId = newCart.Id
+                ShoppingCartId = shoppingCart.Id
             };
-
             _context.CartItems.Add(newCartItem);
             await _context.SaveChangesAsync();
-
-            _response.StatusCode = HttpStatusCode.OK;
-            _response.IsSuccess = true;
-            _response.Result = newCart;
-            return Ok(_response);
         }
-
-        if (shoppingCart is not null)
+        else if (cartItemInCart is not null)
         {
-            var cartItemInCart = shoppingCart.CartItems.FirstOrDefault(x => x.MenuItemId == menuItemId);
-            if (cartItemInCart is null && updateQuantityBy > 0)
+            var newQuantity = cartItemInCart.Quantity + updateQuantityBy;
+            if (updateQuantityBy == 0 || newQuantity <= 0)
             {
-                CartItem newCartItem = new()
+                _context.CartItems.Remove(cartItemInCart);
+
+                if (shoppingCart.CartItems.Count == 1) // removing last item → remove cart too
                 {
-                    MenuItemId = menuItemId,
-                    Quantity = updateQuantityBy,
-                    ShoppingCartId = shoppingCart.Id
-                };
-                _context.CartItems.Add(newCartItem);
-                await _context.SaveChangesAsync();
+                    _context.ShoppingCarts.Remove(shoppingCart);
+                }
             }
-            else if (cartItemInCart is not null)
+            else
             {
-                var newQuantity = cartItemInCart.Quantity + updateQuantityBy;
-                if (updateQuantityBy == 0 || newQuantity <= 0)
-                {
-                    _context.CartItems.Remove(cartItemInCart);
-
-                    if (shoppingCart.CartItems.Count == 1) // removing last item → remove cart too
-                    {
-                        _context.ShoppingCarts.Remove(shoppingCart);
-                    }
-                }
-                else
-                {
-                    cartItemInCart.Quantity = newQuantity;
-                }
-
-                await _context.SaveChangesAsync();
+                cartItemInCart.Quantity = newQuantity;
             }
 
-            _response.StatusCode = HttpStatusCode.OK;
-            _response.IsSuccess = true;
-            _response.Result = shoppingCart;
-            return Ok(_response);
+            await _context.SaveChangesAsync();
         }
 
-        _response.StatusCode = HttpStatusCode.BadRequest;
-        _response.IsSuccess = false;
-        return BadRequest(_response);
+        _response.StatusCode = HttpStatusCode.OK;
+        _response.IsSuccess = true;
+        _response.Result = shoppingCart;
+        return Ok(_response);
     }
+
+    _response.StatusCode = HttpStatusCode.BadRequest;
+    _response.IsSuccess = false;
+    return BadRequest(_response);
+}
 }
